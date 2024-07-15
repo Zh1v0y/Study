5be7386ee5250f2246c42b99046fe3ed73c92ec5
Zh1v0y <65412913+Zh1v0y@users.noreply.github.com>
2024-07-15 15:12:20 +0300

    Надеюсь все правильно

--- src/Lesson_2/Employee.java
+++ src/Lesson_2/Employee.java
@@ -20,35 +20,31 @@ public class Employee {
     // Метод для вывода информации об объекте
     public void printInfo() {
         System.out.println("Employee Information:");
-        System.out.println("FIO: " + FIO);
-        System.out.println("job: " + job);
-        System.out.println("Email: " + email);
-        System.out.println("Phone: " + phone);
-        System.out.println("Salary: " + salary);
-        System.out.println("Age: " + age);
+        System.out.println(" FIO: " + FIO);
+        System.out.println(" job: " + job);
+        System.out.println(" Email: " + email);
+        System.out.println(" Phone: " + phone);
+        System.out.println(" Salary: " + salary  + " $");
+        System.out.println(" Age: " + age);
+        System.out.println(" ");
+
     }
 
-    public int getAge() {
-        return age;
-    }
+
 
     public static void main(String[] args) {
-        Employee employee = new Employee("Абрамович Ольга Викторовна", "QA", "test@test.com", "+375445673423", 800, 23);
+        Employee employee = new Employee("Alberty Alexander", "QA", "test@test.com", "+375445673423", 800, 23);
         employee.printInfo();
         System.out.println("--------------------------");
         Employee[] employeeArray = new Employee[5];
-        employeeArray[0] = new Employee("Иванов Иван Петрович", "Engineer", "ivivan@mailbox.com", "+37544563789", 30000, 30);
-        employeeArray[1] = new Employee("Петров Петр Иванович", "Manager", "petrov@mailbox.com", "+375445673420", 50000, 45);
-        employeeArray[2] = new Employee("Сидоров Олег Викторович", "Director", "sidorov@mailbox.com", "+375445673428", 70000, 50);
-        employeeArray[3] = new Employee("Федоров Дмитрий Петрович", "Developer", "fedorov@mailbox.com", "+375445673426", 60000, 25);
-        employeeArray[4] = new Employee("Смирнов Динис Евгеньевич", "Analyst", "smirnov@mailbox.com", "+375445673425", 40000, 42);
+        employeeArray[0] = new Employee("Allard Allen", "Engineer", "ivivan@mailbox.com", "+37544563789", 3000, 30);
+        employeeArray[1] = new Employee("Anderson Bailiff", "Manager", "petrov@mailbox.com", "+375445673420", 5000, 45);
+        employeeArray[2] = new Employee("Bell Bennett", "Director", "sidorov@mailbox.com", "+375445673428", 7000, 50);
+        employeeArray[3] = new Employee("Biffle Brooks", "Developer", "fedorov@mailbox.com", "+375445673426", 6000, 25);
+        employeeArray[4] = new Employee("Brown Butler", "Analyst", "smirnov@mailbox.com", "+375445673425", 4000, 42);
 
-        // Print information only about employees older than 40 years
-        for (Employee employee1 : employeeArray) {
-            if (employee1.getAge() > 40) {
-                employee1.printInfo();
-            }
-        }
+        employeeArray[0].printInfo();
+
 
     }
 }
